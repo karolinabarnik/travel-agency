f187be008d66f48969a07e208a0534b6eaf782c6
Karolina Barnik <karolinabarnik@gmail.com>
2021-07-21 12:41:15 +0200

    Add orderReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,8 @@ import tripList from '../data/trips.json';
 import globalReducer from './globalRedux';
 import filtersReducer from './filtersRedux';
 
+import orderReducer from './orderRedux';
+
 // define initial state and shallow-merge initial data
 const initialState = {
   trips: tripList,
@@ -18,12 +20,18 @@ const initialState = {
       from: 1,
       to: 14,
     },
-  },
+    order: {
+      trip: null,
+      email: '',
+      options: {},
+    },
+  }, 
 };
 
 // define reducers
 const reducers = {
   filters: filtersReducer,
+  order: orderReducer,
 };
 
 // add blank reducers for initial state properties without reducers
