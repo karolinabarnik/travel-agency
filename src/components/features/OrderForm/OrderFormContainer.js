c3dfde8ec17aaf872c36c5f87eead6c4022d5cca
Karolina Barnik <karolinabarnik@gmail.com>
2021-07-25 22:06:40 +0200

    CalculateTotal and formatPrice

---
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -0,0 +1,14 @@
+import { getOrderOptions, setOrderOption } from '../../../redux/orderRedux';
+import OrderForm from './OrderForm';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  options: getOrderOptions(state),
+  setOrderOption: setOrderOption(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  setOrderOption: option => dispatch(setOrderOption(option)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderForm);
