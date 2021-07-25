c2de1f9048bafd100ab6e32fd47b8d7e2aee6709
Karolina Barnik <karolinabarnik@gmail.com>
2021-07-25 22:37:16 +0200

    Modify OrderOption

---
+++ src/components/features/OrderOptions/OrderOption.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './../OrderOption/OrderOption.scss';
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+import OrderOptionNumber from './OrderOptionNumber';
+
+
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+  
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];
+  if(!OptionComponent){
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}
+        />
+      </div>
+    );
+  }
+};
+  
+
+OrderOption.propTypes = {
+  name: PropTypes.string,
+};
+  
+
+
+export default OrderOption;
