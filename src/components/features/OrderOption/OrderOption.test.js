3d90e5859de2b565e3615485db27c1a71ab9d8c6
Karolina Barnik <karolinabarnik@gmail.com>
2021-08-03 23:12:50 +0200

    Add OrderOption.test

---
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import OrderOption from './OrderOption';
+
+describe('Component OrderOption', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<OrderOption name='Lorem ipsum' />);
+    expect(component).toBeTruthy();
+  
+  });
+
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+
+  it('should render correct title', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const component = shallow(<OrderOption name={expectedTitle} />);
+      
+    const renderedTitle = component.find('.title').text();
+    expect(renderedTitle).toEqual(expectedTitle);
+        
+  });  
+
+}); 
